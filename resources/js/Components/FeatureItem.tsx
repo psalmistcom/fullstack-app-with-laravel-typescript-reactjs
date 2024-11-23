bea72ecb1fecbcd83bddd7c5461efc5dad9b42a9
psalmistcom <dadewale9@gmail.com>
2024-11-23 14:44:40 +0100

    implement comment btn on frontend

--- resources/js/Components/FeatureItem.tsx
+++ resources/js/Components/FeatureItem.tsx
@@ -13,7 +13,7 @@ export default function FeatureItem({ feature }: { feature: Feature }) {
     return (
         <div className="mb-4 overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
             <div className="p-6 text-gray-900 dark:text-gray-100 flex gap-8">
-               <FeatureUpvoteDownvote feature={feature} />
+                <FeatureUpvoteDownvote feature={feature} />
                 <div className="flex-1">
                     <h2 className="text-2xl mb-3">
                         <Link href={route("feature.show", feature)}>
@@ -41,6 +41,13 @@ export default function FeatureItem({ feature }: { feature: Feature }) {
                     {(feature.description || "").length <= 200 && (
                         <p>{feature.description}</p>
                     )}
+
+                    <div>
+                        <Link
+                            className="inline-flex gap-2 py-2.5 px-5 me-2 mb-2 mt-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                            href={route("feature.show", feature)}
+                        >Comments </Link>
+                    </div>
                 </div>
                 <div>
                     <FeatureActionDropdown feature={feature} />
